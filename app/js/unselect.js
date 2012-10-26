c1034346c887192165f4648065f7b486baee452d
jasonnerothin <jasonnerothin@superiorbiomining.com>
2012-10-25 22:16:21 -0500

    missed a delete

--- app/js/unselect.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var evwybody = ['gigs','skills','edu','portfolio','bio'];
-function unselect(){
-    for( var somebunny in evwybody ){
-        $(somebunny).removeClass('selected');
-    }
-}
