ff2b66ba55a5e4aaa90af8c8662f82311eda7bd2
jasonnerothin <jasonnerothin@superiorbiomining.com>
2012-11-24 08:34:01 -0600

    Basic layout for gig detail.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,16 +7,6 @@ function SkillListCtrl($scope, Skill) {
     $scope.skills = Skill.query();
     $scope.orderProp = 'skillName';
     $scope.query = "";
-    // lhs flips the bit, but rhs does not: this way we can call both per iteration
-    // in ng-class...
-    $scope.ltrSwitch = true;
-    $scope.lhs = function(){
-        $scope.ltrSwitch = !$scope.ltrSwitch;
-        return !$scope.ltrSwitch
-    };
-    $scope.rhs = function(){
-        return !$scope.ltrSwitch;
-    };
     $scope.hasLink = function(skill){
         return angular.isDefined(skill.infoLink);
     };
@@ -53,16 +43,23 @@ function GigListCtrl($scope, Gig) {
 function GigDetailCtrl($scope, $routeParams, Gig) {
     $scope.gig = Gig.get({gigId:$routeParams.gigId}, function (gig) {
         $scope.gigId = gig.gigId;
+        $scope.gigName = gig.gigName;
         $scope.title = gig.title;
         $scope.employer = gig.employer;
-        $scope.address = gig.address;
-        $scope.methodology = gig.methodology;
+        $scope.employerUrl = gig.employerUrl;
         $scope.blurb = gig.blurb;
-        $scope.results = gig.results;
+        $scope.cityState = gig.cityState;
+        $scope.methodology = gig.methodology;
+        $scope.result = gig.result;
         $scope.startDate = gig.startDate;
         $scope.endDate = gig.endDate;
         $scope.skills = gig.skills;
     });
+
+    $scope.isOverWith = function(){
+        return $scope.gig.endDate == 0;
+    };
+
 }
 
 function BioCtrl($scope) {
