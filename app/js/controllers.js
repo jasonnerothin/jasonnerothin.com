98fd8c12cf622245209f83a20af5d8e9652d2e4b
jasonnerothin <jasonnerothin@superiorbiomining.com>
2012-11-26 11:48:08 -0600

    Refining gig detail.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,15 +4,19 @@
 
 /* About my valuable skills... */
 function SkillListCtrl($scope, Skill) {
+
     $scope.skills = Skill.query();
     $scope.orderProp = 'skillName';
     $scope.query = "";
+
     $scope.hasLink = function(skill){
         return angular.isDefined(skill.infoLink);
     };
+
 }
 
 function SkillDetailCtrl($scope, $routeParams, Skill) {
+
     $scope.skill = Skill.get({skillId:$routeParams.skillId}, function (skill) {
         $scope.skillName = skill.skillName;
         $scope.skillId = skill.skillId;
@@ -23,24 +27,30 @@ function SkillDetailCtrl($scope, $routeParams, Skill) {
         $scope.categories = skill.categories;
         $scope.monthsExperience = skill.monthsExperience;
     });
+
     $scope.hasLink = function(){
         var def = angular.isDefined(Skill.infoLink);
         var hl = def && Skill.infoLink.length > 0 ;
         return hl;
     };
+
 }
 
 /* About the great gigs I've worked... */
 function GigListCtrl($scope, Gig) {
+
     $scope.gigs = Gig.query();
     $scope.orderProp = 'name';
     $scope.query = "";
+
     $scope.isOverWith = function(idx){
         return $scope.gigs[idx].endDate == 0;
     };
+
 }
 
 function GigDetailCtrl($scope, $routeParams, Gig) {
+
     $scope.gig = Gig.get({gigId:$routeParams.gigId}, function (gig) {
         $scope.gigId = gig.gigId;
         $scope.gigName = gig.gigName;
@@ -54,14 +64,41 @@ function GigDetailCtrl($scope, $routeParams, Gig) {
         $scope.startDate = gig.startDate;
         $scope.endDate = gig.endDate;
         $scope.skills = gig.skills;
+
+        $scope.contributions = gig.contributions;
+        $scope.leadership = gig.leadership;
+        $scope.sdlcProcess = gig.sdlcProcess;
     });
 
     $scope.isOverWith = function(){
         return $scope.gig.endDate == 0;
     };
 
+    $scope.contributed = function(){
+        if( !angular.isDefined($scope.gig.contributions) ) return false;
+        var len = $scope.gig.contributions.length;
+        return len > 0;
+    };
+
+    $scope.lead = function(){
+        if( !angular.isDefined($scope.gig.leadership) ) return false;
+        var len = $scope.gig.leadership.length;
+        return len > 0;
+    };
+
+    $scope.team = function(){
+        var goTeam = angular.isDefined($scope.gig.team);
+        return goTeam;
+    };
+
+    $scope.sdlc = function(){
+        var sdlcProc = angular.isDefined($scope.gig.sdlcProcess);
+        return sdlcProc;
+    };
+
 }
 
+/* empty controllers for future use... */
 function BioCtrl($scope) {
 }
 
