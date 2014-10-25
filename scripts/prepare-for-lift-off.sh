0e8923bc758cc15ddd89db72966fe8a684ee8fe1
Jason Nerothin <jasonnerothin@gmail.com>
2014-10-24 22:30:21 -0500

    whack job

--- scripts/prepare-for-lift-off.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-find scripts -type f -name "*test*" -exec rm {} \;
-find . -type d -name "*test*" -exec rm -rf {} \;
-find . -type f -name "*.iml" -exec rm {} \;
-find . -name "*git*" -exec rm -rf {} \;
-rm -rf scripts/private
-rm -rf config README.md .idea
